b98af4088c4a4368971219eaee805fd1ebb61b50
remicmacs <rembourgeon@gmail.com>
2019-03-28 22:44:12 +0100

    Add key value pair syntax to `export`

--- ExportBuiltin.cpp
+++ ExportBuiltin.cpp
@@ -3,6 +3,9 @@
 void ExportBuiltin::exec_base() {
     // TODO: Maybe factor code in a class to execute external commands?
     // Export alone == `env`
+    // It's weird to do that
+    // TODO: `export -p` syntax == same as `env` but with `export` prefix on
+    // each key/value pair. Maybe do it properly without call to exec ?
     if (this->get_args().empty()) {
         // make a fork
         pid_t pid = fork();
@@ -32,6 +35,23 @@ void ExportBuiltin::exec_base() {
 
             cout << WEXITSTATUS(status);
         }
+    } else { // Export Key=value pair
+        string key;
+        string value;
+        string delimiter = "=";
+
+        string key_value_pair = this->get_args()[0];
+
+        string::size_type pos = key_value_pair.find(delimiter);
+
+        // No delimiter found in key / value pair
+        if (pos == string::npos) return;
+
+        key = key_value_pair.substr(0, pos);
+        value = key_value_pair.substr(pos+1, string::npos);
+
+        // Overwrite existing value
+        setenv(key.c_str(), value.c_str(), 1);
     }
     return;
 }
